900c7c8339b0cb4d86755b1c49574a46e9b4fdfe
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-05-12 20:58:49 +0200

    Delete src directory

--- src/js/task-01.js
+++
@@ -1,7 +0,0 @@
-const items = document.querySelectorAll('li.item');
-console.log( `Number of categories: ${items.length}`);
-const allCategories = document.querySelectorAll('ul#categories li.item');
-allCategories.forEach(item => {
-    console.log( `Category : ${item.querySelector('h2').textContent}`);
-    console.log (`Elements: ${item.querySelectorAll('li').length}`);
-});
