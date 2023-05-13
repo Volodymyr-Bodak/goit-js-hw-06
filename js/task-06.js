0817619833448a07a23bcb28d103e0f50e15eb2b
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-05-13 16:44:05 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,13 @@
 const input = document.querySelector('#validation-input[data-length="6"]');
-const number = input.dataset.length;
 input.addEventListener('blur', () =>{
   const dataLength = input.getAttribute('data-length');
 
   if (input.value.length === Number(dataLength)) {
         input.classList.add('valid');
-        
+        input.classList.remove('invalid');
     } else {
         input.classList.add('invalid');
+        input.classList.remove('valid');
     }
 
 });
-console.log(number)
