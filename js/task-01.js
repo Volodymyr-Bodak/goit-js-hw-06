40b6967a1aecf59ccb2424ae6e8dd06132d83c2e
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-05-13 16:42:28 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,12 @@
-const items = document.querySelectorAll('li.item');
-console.log( `Number of categories: ${items.length}`);
-const allCategories = document.querySelectorAll('ul#categories li.item');
-allCategories.forEach(item => {
-    console.log( `Category : ${item.querySelector('h2').textContent}`);
-    console.log (`Elements: ${item.querySelectorAll('li').length}`);
-});
+const categoriesList = document.getElementById('categories');
+const categoriesItems = categoriesList.children;
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+for (let i = 0; i < categoriesItems.length; i++) {
+  const categoryItem = categoriesItems[i];
+  const categoryTitle = categoryItem.firstElementChild.textContent;
+  const categoryItemsCount = categoryItem.querySelectorAll('li').length ;
+
+  console.log(`Category: ${categoryTitle} \nNumber of items: ${categoryItemsCount}`);
+}
